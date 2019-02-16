814227c193c5e06c8598ecfde3e53300fceabd94
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-02-16 20:36:03 +0400

    Небольшие изменения

--- Labs/1/flipbyte/flipbyte/flipbyte.cpp
+++ Labs/1/flipbyte/flipbyte/flipbyte.cpp
@@ -7,14 +7,13 @@ using namespace std;
 uint8_t GetReverseByte(uint8_t byte)
 {
 	const int NUMBER_OF_BITS = 8;
-	const uint8_t BIT_MASK_1 = 0x01;
-	const uint8_t BIT_MASK_2 = 0x80;
+	const uint8_t BIT_MASK = 0x01;
 	uint8_t reverseByte = 0;
 	for (int i = 0; i < NUMBER_OF_BITS; i++)
 	{
-		if ((byte & (BIT_MASK_1 << i)) != 0)
+		if ((byte & (BIT_MASK << i)) != 0)
 		{
-			reverseByte = reverseByte | (BIT_MASK_2 >> i);
+			reverseByte = reverseByte | (BIT_MASK << NUMBER_OF_BITS - 1 - i);
 		}
 	}
 	return reverseByte;
