0ab8b6735d7e690c620e1cf4841f12e14fc48d98
Vladimir Stepanov <vovan.number.one@mail.ru>
2019-03-06 15:40:16 +0400

    2-ая лаба 2-ое задание

---
+++ Labs/2/HTMLDecode/HTMLDecode/HTMLDecode.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct HtmlCharacter
+{
+	char ch;
+	string code;
+};
+
+const HtmlCharacter QUOT = { '"', "&quot;" };
+const HtmlCharacter APOS = { '\'', "&apos;" };
+const HtmlCharacter LT = { '<', "&lt;" };
+const HtmlCharacter GT = { '>', "&gt;" };
+const HtmlCharacter AMP = { '&', "&amp;" };
+
+string GetChangedStr(const string& str, const string& searchStr, const char& replaceCh)
+{
+	string changedStr;
+	int index = 0;
+	int foundIndex;
+	while (index < str.length())
+	{
+		foundIndex = str.find(searchStr, index);
+		if (foundIndex != -1 && searchStr.length() > 0)
+		{
+			changedStr.append(str, index, foundIndex - index);
+			changedStr += replaceCh;
+			index = foundIndex + searchStr.length();
+		}
+		else
+		{
+			changedStr.append(str, index);
+			break;
+		}
+	}
+	return changedStr;
+}
+
+string HtmlDecode(const string &html)
+{
+	string text = GetChangedStr(html, QUOT.code, QUOT.ch);
+	text = GetChangedStr(text, APOS.code, APOS.ch);
+	text = GetChangedStr(text, LT.code, LT.ch);
+	text = GetChangedStr(text, GT.code, GT.ch);
+	return GetChangedStr(text, AMP.code, AMP.ch);
+}
+
+int main()
+{
+	string html;
+	while (getline(cin, html))
+	{
+		cout << HtmlDecode(html);
+		if (!cin.eof())
+		{
+			cout << endl;
+		}
+	}
+}
